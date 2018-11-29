b4c90d27fe96de3494d21be9d96f26dfa9af5484
Aleksandr Mironov <aleksandr.mironov@simbirsoft.com>
2018-11-29 16:36:12 +0300

    2

---
+++ app/src/main/java/com/example/igor/androidtask2/adapter/CategoriesAdapter.java
@@ -0,0 +1,60 @@
+package com.example.igor.androidtask2.adapter;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.igor.androidtask2.R;
+import com.example.igor.androidtask2.entity.CategoryEntity;
+
+import java.util.ArrayList;
+
+public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.CategoriesViewHolder> {
+
+    //private ArrayList<Drawable> categoryImage;
+    //private ArrayList<String> categoryText;
+    private ArrayList<CategoryEntity> listCategories;
+
+    public CategoriesAdapter(ArrayList<CategoryEntity> listCategories) {
+        this.listCategories = listCategories;
+    }
+
+    @NonNull
+    @Override
+    public CategoriesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.categories_item_view, parent, false);
+        return new CategoriesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoriesViewHolder holder, int position) {
+        holder.bindItem(listCategories.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listCategories.size();
+    }
+
+    class CategoriesViewHolder extends RecyclerView.ViewHolder{
+        private ImageView categoriesImageView;
+        private TextView categoriesTextView;
+
+        public CategoriesViewHolder(View itemView) {
+            super(itemView);
+            categoriesImageView = itemView.findViewById(R.id.image_categories);
+            categoriesTextView = itemView.findViewById(R.id.text_categories);
+        }
+
+        public void bindItem(CategoryEntity item){
+            categoriesImageView.setImageDrawable(item.getDrawableImage());
+            categoriesTextView.setText(item.getText());
+        }
+    }
+}
---
+++ app/src/main/java/com/example/igor/androidtask2/entity/CategoryEntity.java
@@ -0,0 +1,30 @@
+package com.example.igor.androidtask2.entity;
+
+import android.graphics.drawable.Drawable;
+
+public class CategoryEntity {
+
+    private Drawable drawableImage;
+    private String text;
+
+    public CategoryEntity(Drawable drawableImage, String text) {
+        this.drawableImage = drawableImage;
+        this.text = text;
+    }
+
+    public Drawable getDrawableImage() {
+        return drawableImage;
+    }
+
+    public void setDrawableImage(Drawable drawableImage) {
+        this.drawableImage = drawableImage;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
